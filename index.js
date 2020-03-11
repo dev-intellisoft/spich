dfcf13d45b78ef9a1e771a107dac40fdea9b4b52
Wellington Cunha <wellington@programer.com.br>
2020-03-11 11:54:39 -0300

    exporting logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import controller from './src/core/controller'
 import model from './src/core/model'
 import framework from './src/core/framework'
+import logger from './src/core/logger'
 
-export { framework, controller, model }
+export { framework, controller, model, logger }
