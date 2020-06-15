a7b931c1805a0bb96a0abe0f24b7c0d8a87f2498
Wellington Cunha <wellington@programer.com.br>
2020-06-15 00:35:43 -0300

    version 3.3.2

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import controller from './src/core/controller'
 import model from './src/core/model'
 import spich from './src/core/spich'
 import logger from './src/core/logger'
+import input from './src/core/input'
+import loader from './src/core/loader'
 
-export { spich, controller, model, logger }
+export { spich, controller, model, logger, input, loader }
