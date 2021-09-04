d4a5c4ef4c793ea7e13eacab0ab52f0e0fde1a63
Wellington Cunha <wellington@programer.com.br>
2021-09-04 15:21:08 +0400

    starting auth test project.

---
+++ example/private/config.js
@@ -0,0 +1,27 @@
+export const config = {
+    /**
+     * Database settings
+     * databases:[
+     * {
+     *       name: `my connection`,
+     *       driver:<postgres | mysql | sqlite>,
+     *       host:`localhost`,
+     *       user:`test`,
+     *       password:`test`,
+     *       database:`test`,
+     *       port:123
+     *   }
+     * ],
+     */
+    databases:[
+    ],
+    server_port:80,
+    /**
+     * authentication: {
+     *    database:`sqlite0`,
+     * }
+     */
+    // authentication: {
+    //     database:`sqlite0`,
+    // }
+}
